758acc23b0e9ef38db23a54f137aecf03432b85b
Paolo Sivila <paspau2@hotmail.com>
2016-03-06 05:15:58 -0430

    Added Boilerplate Logo in Menu State

--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,7 @@ var menuState = {
     create: function () {
 
         game.add.plugin(Phaser.Plugin.Debug);
-        game.add.image(15, 80, 'Dall');
+        game.add.image(15, 80, 'boiler-logo');
         game.add.plugin(Phaser.Plugin.Inspector);
 
     }
