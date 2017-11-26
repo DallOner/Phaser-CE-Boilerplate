3da7c2f0175f29afe9f983f9e617b22fd8235a54
DallOner <paspau2@hotmail.com>
2017-11-26 13:28:34 -0500

    Added Phaser input plugin

--- js/menu.js
+++ js/menu.js
@@ -3,8 +3,11 @@ var menuState = {
     create: function () {
 
         game.add.plugin(Phaser.Plugin.Debug);
-        game.add.image(95, 150, 'boiler-logo');
         game.add.plugin(Phaser.Plugin.Inspector);
+        game.add.plugin(PhaserSuperStorage.StoragePlugin);
+        game.add.plugin(PhaserInput.Plugin);
+        
+        game.add.image(95, 150, 'boiler-logo');
 
     }
 
